34f39259deb06287031937e03f63e3099c0193e2
Hassan Fakih <98998476+HasanFakih21@users.noreply.github.com>
2026-07-15 14:02:24 -0400

    Remove Network File (#69)
    Bench: 853357

---
+++ build.rs
@@ -0,0 +1,23 @@
+use std::{fs, process::Command};
+
+const BASE_URL: &str =
+    "https://github.com/HasanFakih21/JustBot-Networks/releases/download/Networks";
+const NETWORK_NAME: &str = "512-HL.nnue";
+
+fn main() {
+    download_netowrk();
+}
+
+fn download_netowrk() {
+    let output = Command::new("curl")
+        .args(["-s", "-O", "-L"])
+        .arg(format!("{BASE_URL}/{NETWORK_NAME}"))
+        .output()
+        .expect("Error executing 'curl'!");
+
+    if output.status.success() {
+        fs::rename(NETWORK_NAME, "model.nnue").expect("Error renaming file!");
+    } else {
+        panic!("Error downloading network!");
+    }
+}
