4a3a40e2f17b9d3ae8df3e573756bb1f8792b414
hasanfakih21 <hasanfakih@cmail.carleton.ca>
2026-08-16 21:19:55 -0400

    bench: 7918979

--- src/search.rs
+++ src/search.rs
@@ -247,7 +247,10 @@ pub fn search<Node: NodeType>(
         .filter(|m| !m.is_null());
     let tt_bound = tt_entry.as_ref().map(|e| e.get_bound());
     let tt_score = tt_entry.as_ref().map(|e| e.get_score());
-    let tt_pv = tt_entry.as_ref().map(|e| e.is_pv()).unwrap_or(false);
+    let tt_pv = tt_entry
+        .as_ref()
+        .map(|e| e.is_pv() || Node::PV)
+        .unwrap_or(Node::PV);
     let tt_depth = tt_entry.as_ref().map(|e| e.get_depth());
 
     // Razoring
@@ -584,7 +587,7 @@ pub fn search<Node: NodeType>(
             data.board.state.keys.full,
             depth,
             ply,
-            Node::PV,
+            tt_pv,
         );
     }
 
