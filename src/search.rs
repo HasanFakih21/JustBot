d4fdb16c7f4d6936bea8991cf24ea008678fa249
hasanfakih21 <hasanfakih@cmail.carleton.ca>
2026-08-17 19:23:34 -0400

    bench: 9540962

--- src/search.rs
+++ src/search.rs
@@ -109,7 +109,7 @@ pub fn search_runner(data: &mut SearchData) {
             let node_scale = (2.977
                 - (data.root_moves[0].nodes as f32 / data.nodes() as f32) * 2.495)
                 .max(0.553);
-            let m_stability_scale = (1.300 - 0.045 * move_stability as f32).max(0.715);
+            let m_stability_scale = (1.800 - 0.010 * move_stability as f32).max(0.900);
             node_scale * m_stability_scale
         };
 
