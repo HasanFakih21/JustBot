dc82147333b8e0882842558cff71f39d39793a51
hasanfakih21 <hasanfakih@cmail.carleton.ca>
2026-08-17 02:56:23 -0400

    bench: 9923375

--- src/search.rs
+++ src/search.rs
@@ -351,6 +351,10 @@ pub fn search<Node: NodeType>(
                 + (singular_score < singular_beta - double_margin) as i32
                 + (singular_score < singular_beta - triple_margin) as i32;
         }
+        // Multi-Cut
+        else if singular_score >= beta && !is_decisive(singular_score) {
+            return singular_score;
+        }
         // Negative Extensions
         else if tt_score >= beta || cutnode {
             extension -= 2;
