da2ab6c4e942343c97f1a40d7d90cdcf06dadb12
hasanfakih21 <hasanfakih@cmail.carleton.ca>
2026-08-21 21:52:46 -0400

    bench: 10610811

--- src/search.rs
+++ src/search.rs
@@ -454,6 +454,7 @@ pub fn search<Node: NodeType>(
             r += 447 * (tt_score.is_some_and(|s| s <= alpha)) as i32;
             r += 296 * (tt_depth.is_some_and(|d| d < depth)) as i32;
             r -= 449 * history / 4096;
+            r -= 850 * is_direct_check as i32;
 
             let reduction = r / 1024;
             let reduced_depth = (new_depth - reduction).max(1) + Node::PV as i32;
