c1eb9119c85852c05dbcbfab99742ee7d669dff5
hasanfakih21 <hasanfakih@cmail.carleton.ca>
2026-08-23 01:35:32 -0400

    bench: 11321113

--- build.rs
+++ build.rs
@@ -2,7 +2,7 @@ use std::{fs, process::Command};
 
 const BASE_URL: &str =
     "https://github.com/HasanFakih21/JustBot-Networks/releases/download/Networks";
-const NETWORK_NAME: &str = "346-v2.nnue";
+const NETWORK_NAME: &str = "409-v1.nnue";
 
 fn main() {
     download_netowrk();
