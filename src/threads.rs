975aac6e09cccee5197dd9719a5ee75f78be9351
hasanfakih21 <hasanfakih@cmail.carleton.ca>
2026-08-28 15:53:58 -0400

    bench: 11094245

--- src/threads.rs
+++ src/threads.rs
@@ -1,3 +1,11 @@
+use std::{
+    sync::{
+        Arc,
+        mpsc::{Receiver, Sender},
+    },
+    thread::JoinHandle,
+};
+
 use crate::{
     board::Board,
     search::{
@@ -9,26 +17,34 @@ use crate::{
 };
 
 pub struct SearchThreads {
-    threads: Vec<SearchData>,
+    workers: Vec<Worker>,
+    shared: Arc<SharedData>,
+}
+
+#[derive(Debug, Clone)]
+pub struct SearchParams {
+    pub board: Board,
+    pub root_moves: Vec<RootMove>,
+    pub time: TimeManager,
+    pub report: Report,
 }
 
 impl SearchThreads {
-    pub fn new(shared: std::sync::Arc<SharedData>, count: usize) -> Self {
-        let mut threads = Vec::new();
-        for id in 0..count {
-            threads.push(SearchData::new(shared.clone(), id));
-        }
+    pub fn new(shared: Arc<SharedData>, count: usize) -> Self {
+        let workers = (0..count)
+            .map(|id| create_worker(Arc::clone(&shared), id))
+            .collect();
 
-        SearchThreads { threads }
+        SearchThreads { workers, shared }
     }
 
     pub fn start(&mut self, board: &Board, mut time: TimeManager, report: Report) -> Option<Move> {
-        debug_assert!(!self.threads.is_empty());
+        debug_assert!(!self.workers.is_empty());
 
         time.set_time_limits(board.state.side_to_move, board.state.full_move);
-        self.threads[0].shared.tt.increase_age();
-        self.threads[0].shared.reset_all_nodes();
-        self.threads[0].shared.status.run();
+        self.shared.tt.increase_age();
+        self.shared.reset_all_nodes();
+        self.shared.status.run();
 
         let root_moves: Vec<RootMove> = board
             .generate_moves(crate::board::movegen::MoveGenKind::All)
@@ -39,31 +55,93 @@ impl SearchThreads {
             })
             .collect();
 
-        std::thread::scope(|scope| {
-            let mut handles = Vec::new();
-            for t in self.threads.iter_mut() {
-                if t.id == 0 {
-                    t.report = report;
-                }
+        let params = SearchParams {
+            board: board.clone(),
+            root_moves,
+            time,
+            report,
+        };
 
-                t.board = board.clone();
-                t.time = time.clone();
-                t.root_moves = root_moves.clone();
+        for w in self.workers.iter_mut() {
+            w.comm
+                .send(Command::Search(Box::new(params.clone())))
+                .expect("Worker not found");
+        }
 
-                handles.push(scope.spawn(|| search_runner(t)));
+        let mut result = None;
+        for (id, w) in self.workers.iter().enumerate() {
+            let r = w.result.recv().expect("Worker not found");
+            if id == 0 {
+                result = r;
             }
+        }
 
-            for handle in handles {
-                let _ = handle.join();
-            }
-        });
-
-        self.threads[0].best_move
+        result
     }
 
     pub fn count(&self) -> usize {
-        self.threads.len()
+        self.workers.len()
     }
+}
+
+impl Drop for SearchThreads {
+    fn drop(&mut self) {
+        self.shared.status.stop();
+        for w in self.workers.iter() {
+            let _ = w.comm.send(Command::Quit);
+        }
+
+        self.workers
+            .drain(..)
+            .for_each(|w| w.handle.join().unwrap());
+    }
+}
+
+fn create_worker(shared: Arc<SharedData>, id: usize) -> Worker {
+    let (cmd_tx, cmd_rx) = std::sync::mpsc::channel();
+    let (result_tx, result_rx) = std::sync::mpsc::channel();
+
+    let handle = std::thread::spawn(move || {
+        let mut data = SearchData::new(Arc::clone(&shared), id);
+
+        while let Ok(command) = cmd_rx.recv() {
+            match command {
+                Command::Search(params) => {
+                    data.board = params.board;
+                    data.root_moves = params.root_moves;
+                    data.time = params.time;
+                    if id == 0 {
+                        data.report = params.report;
+                    } else {
+                        data.report = Report::None;
+                    }
+
+                    search_runner(&mut data);
+                    if result_tx.send(data.best_move).is_err() {
+                        break;
+                    };
+                }
+                Command::Quit => break,
+            }
+        }
+    });
+
+    Worker {
+        handle,
+        comm: cmd_tx,
+        result: result_rx,
+    }
+}
+
+enum Command {
+    Search(Box<SearchParams>),
+    Quit,
+}
+
+struct Worker {
+    handle: JoinHandle<()>,
+    comm: Sender<Command>,
+    result: Receiver<Option<Move>>,
 }
 
 #[cfg(test)]
