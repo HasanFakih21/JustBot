e93f971090b6971b112bc4160d6ac8b6ace6afd3
hasanfakih21 <hasanfakih@cmail.carleton.ca>
2026-08-28 22:39:05 -0400

    bench: 9233941

--- src/types/constants.rs
+++ src/types/constants.rs
@@ -70,12 +70,12 @@ pub static LMR_TABLE: LazyLock<Box<[[[i32; 64]; 128]; 2]>> = {
         for depth in 1..128 {
             for move_count in 1..64 {
                 // Quiet Moves
-                table[1][depth][move_count] = ((0.5491
-                    + (move_count as f32).ln() * (depth as f32).ln() / 2.4498)
+                table[1][depth][move_count] = ((0.5595
+                    + (move_count as f32).ln() * (depth as f32).ln() / 2.4581)
                     * 1024.0) as i32;
                 // Noisy Moves
-                table[0][depth][move_count] = ((0.0956
-                    + (move_count as f32).ln() * (depth as f32).ln() / 2.9950)
+                table[0][depth][move_count] = ((0.1079
+                    + (move_count as f32).ln() * (depth as f32).ln() / 3.0121)
                     * 1024.0) as i32;
             }
         }
