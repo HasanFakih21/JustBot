37945ac7fe403a56119c3ff34b289dcee34b0282
hasanfakih21 <hasanfakih@cmail.carleton.ca>
2026-08-29 15:09:43 -0400

    bench: 11094245

--- build.rs
+++ build.rs
@@ -1,11 +1,33 @@
-use std::{fs, process::Command};
+use std::{
+    env,
+    path::{Path, PathBuf},
+    process::Command,
+};
 
 const BASE_URL: &str =
     "https://github.com/HasanFakih21/JustBot-Networks/releases/download/Networks";
 const NETWORK_NAME: &str = "540-v1-1024HL.nnue";
 
 fn main() {
-    download_netowrk();
+    set_model_env_var();
+    let network_path = Path::new(env!("CARGO_MANIFEST_DIR")).join(NETWORK_NAME);
+    if !network_path.exists() && env::var("EVALFILE").is_err() {
+        download_netowrk();
+    }
+
+    println!("cargo::rerun-if-changed={}", network_path.display());
+    println!("cargo::rerun-if-env-changed=EVALFILE")
+}
+
+fn set_model_env_var() {
+    let net = env::var("EVALFILE").unwrap_or(NETWORK_NAME.to_string());
+    let mut path = PathBuf::from(net);
+
+    if path.is_relative() {
+        path = Path::new(env!("CARGO_MANIFEST_DIR")).join(path);
+    }
+
+    println!("cargo::rustc-env=MODEL={}", path.display());
 }
 
 fn download_netowrk() {
@@ -15,9 +37,7 @@ fn download_netowrk() {
         .output()
         .expect("Error executing 'curl'!");
 
-    if output.status.success() {
-        fs::rename(NETWORK_NAME, "model.nnue").expect("Error renaming file!");
-    } else {
+    if !output.status.success() {
         panic!("Error downloading network!");
     }
 }
