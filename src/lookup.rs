fee3e6f946cf9279e97ff4920cf93a4e0f33f37d
hasanfakih21 <hasanfakih@cmail.carleton.ca>
2026-08-29 20:51:03 -0400

    bench: 10865879

--- src/lookup.rs
+++ src/lookup.rs
@@ -21,42 +21,46 @@ pub fn attacks(side: Side, square: Square, piece: Piece, occupancies: BitBoard) 
     }
 }
 
-pub const fn pawn_attacks(square: Square, side: Side) -> BitBoard {
-    PAWN_ATTACKS[side as usize][square as usize]
+pub fn pawn_attacks(square: Square, side: Side) -> BitBoard {
+    unsafe {
+        *PAWN_ATTACKS
+            .get_unchecked(side as usize)
+            .get_unchecked(square as usize)
+    }
 }
 
-pub const fn knight_attacks(square: Square) -> BitBoard {
-    KNIGHT_ATTACKS[square as usize]
+pub fn knight_attacks(square: Square) -> BitBoard {
+    unsafe { *KNIGHT_ATTACKS.get_unchecked(square as usize) }
 }
 
-pub const fn king_attacks(square: Square) -> BitBoard {
-    KING_ATTACKS[square as usize]
+pub fn king_attacks(square: Square) -> BitBoard {
+    unsafe { *KING_ATTACKS.get_unchecked(square as usize) }
 }
 
 pub fn bishop_attacks(square: Square, board_occupancy: BitBoard) -> BitBoard {
-    let occupancy = board_occupancy & BISHOP_MASKS[square as usize];
+    let occupancy = board_occupancy & unsafe { *BISHOP_MASKS.get_unchecked(square as usize) };
     let magic_index = magic_index(
         occupancy,
-        BISHOP_OCCUPANCY_BIT_COUNTS[square as usize],
-        BISHOP_MAGIC_NUMBERS[square as usize],
+        unsafe { *BISHOP_OCCUPANCY_BIT_COUNTS.get_unchecked(square as usize) },
+        unsafe { *BISHOP_MAGIC_NUMBERS.get_unchecked(square as usize) },
     );
 
     let offset = (square as usize * 512) + magic_index;
 
-    BISHOP_ATTACKS[offset]
+    unsafe { *BISHOP_ATTACKS.get_unchecked(offset) }
 }
 
 pub fn rook_attacks(square: Square, board_occupancy: BitBoard) -> BitBoard {
-    let occupancy = board_occupancy & ROOK_MASKS[square as usize];
+    let occupancy = board_occupancy & unsafe { *ROOK_MASKS.get_unchecked(square as usize) };
     let magic_index = magic_index(
         occupancy,
-        ROOK_OCCUPANCY_BIT_COUNTS[square as usize],
-        ROOK_MAGIC_NUMBERS[square as usize],
+        unsafe { *ROOK_OCCUPANCY_BIT_COUNTS.get_unchecked(square as usize) },
+        unsafe { *ROOK_MAGIC_NUMBERS.get_unchecked(square as usize) },
     );
 
     let offset = (square as usize * 4096) + magic_index;
 
-    ROOK_ATTACKS[offset]
+    unsafe { *ROOK_ATTACKS.get_unchecked(offset) }
 }
 
 pub fn queen_attacks(square: Square, board_occupancy: BitBoard) -> BitBoard {
