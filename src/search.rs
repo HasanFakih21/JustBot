70d3e310aba8779573766acb92e441fe17815bcd
hasanfakih21 <hasanfakih@cmail.carleton.ca>
2026-08-31 17:11:21 -0400

    bench: 15736528

--- src/search.rs
+++ src/search.rs
@@ -350,7 +350,8 @@ pub fn search<Node: NodeType>(
         && tt_bound != Bound::Upper
     {
         let singular_depth = (depth - 1) / 2;
-        let singular_beta = tt_score - (depth + depth);
+        let singular_margin = depth + depth * (tt_bound != Bound::Exact) as i32;
+        let singular_beta = tt_score - singular_margin;
 
         data.stack[ply].excluded = tt_move;
         data.stack[ply].m = Move::default();
