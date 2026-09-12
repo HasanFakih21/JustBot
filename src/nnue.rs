dae622a34f08aa9adf2f2bf086142534401dbab6
hasanfakih21 <hasanfakih@cmail.carleton.ca>
2026-09-12 05:17:48 -0400

    bench: 8331965

--- src/nnue.rs
+++ src/nnue.rs
@@ -34,17 +34,17 @@ const QB: i16 = 64;
 
 #[rustfmt::skip]
 const BUCKET_LAYOUT: [usize; 32] = [
-    0, 0, 1, 1, 
-    2, 2, 2, 2,
-    3, 3, 3, 3, 
-    3, 3, 3, 3,
-    3, 3, 3, 3,
-    3, 3, 3, 3, 
-    3, 3, 3, 3,
-    3, 3, 3, 3,
+    0, 1, 2, 3,
+    4, 4, 5, 5,
+    6, 6, 6, 6,
+    7, 7, 7, 7,
+    7, 7, 7, 7,
+    7, 7, 7, 7,
+    7, 7, 7, 7,
+    7, 7, 7, 7,
 ];
 
-const NUM_INPUT_BUCKETS: usize = 4;
+const NUM_INPUT_BUCKETS: usize = 8;
 
 pub static MODEL: Parameters = unsafe { std::mem::transmute(*include_bytes!(env!("MODEL"))) };
 
