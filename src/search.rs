e516076a818cd7eb71ba9431702c0f37188872fb
hasanfakih21 <hasanfakih@cmail.carleton.ca>
2026-09-20 02:41:31 -0400

    bench: 7652504

--- src/search.rs
+++ src/search.rs
@@ -519,7 +519,8 @@ pub fn search<Node: NodeType>(
                 && !is_direct_check
                 && !is_win(beta)
                 && is_quiet
-                && move_count as i32 > (2976 + (1363 + 263 * improving as i32) * depth * depth) / 1024
+                && move_count as i32
+                    > (2976 + (1363 + 263 * improving as i32) * depth * depth + 50 * history / 1024) / 1024
             {
                 skip_quiets = true;
                 continue;
