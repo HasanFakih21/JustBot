87c946b85e364b13aeac190ace5e5643d2808e64
hasanfakih21 <hasanfakih@cmail.carleton.ca>
2026-09-20 16:03:34 -0400

    bench: 7381822

--- build.rs
+++ build.rs
@@ -5,7 +5,7 @@ use std::{
 };
 
 const BASE_URL: &str = "https://github.com/HasanFakih21/JustBot-Networks/releases/download/Networks";
-const NETWORK_NAME: &str = "754a4ib-1024.nnue";
+const NETWORK_NAME: &str = "754b6ib-1024.nnue";
 
 fn main() {
     set_model_env_var();
